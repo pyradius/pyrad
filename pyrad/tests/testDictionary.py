595adcdb4aa2e113eb15c72ed7735786d4edfd0c
Wichert Akkerman <wichert@wiggy.net>
2007-09-02 14:39:54 +0000

    Test value parsing

--- pyrad/tests/testDictionary.py
+++ pyrad/tests/testDictionary.py
@@ -36,13 +36,13 @@ class DictionaryInterfaceTests(unittest.TestCase):
 class DictionaryParsingTests(unittest.TestCase):
     def setUp(self):
         self.path=os.path.join(home, "tests", "data")
+        self.dict=Dictionary(os.path.join(self.path, "simple"))
 
     def testParseEmptyDictionary(self):
         dict=Dictionary(StringIO(""))
         self.assertEqual(len(dict), 0)
 
     def testParseSimpleDictionary(self):
-        dict=Dictionary(os.path.join(self.path, "simple"))
         self.assertEqual(len(dict), 8)
         values = [
                 ( "Test-String", 1, "string" ),
@@ -61,8 +61,7 @@ class DictionaryParsingTests(unittest.TestCase):
             self.assertEqual(attr.type, type)
 
     def testAttributeTooFewColumnsError(self):
-        dict=Dictionary()
-        self.assertRaises(ParseError, dict.ReadDictionary,
+        self.assertRaises(ParseError, self.dict.ReadDictionary,
                 StringIO("ATTRIBUTE Oops-Too-Few-Columns"))
         try:
             dict.ReadDictionary(StringIO("ATTRIBUTE Oops-Too-Few-Columns"))
@@ -72,53 +71,48 @@ class DictionaryParsingTests(unittest.TestCase):
 
 
     def testAttributeUnknownTypeError(self):
-        dict=Dictionary()
-        self.assertRaises(ParseError, dict.ReadDictionary,
+        self.assertRaises(ParseError, self.dict.ReadDictionary,
                 StringIO("ATTRIBUTE Test-Type 1 dummy"))
         try:
-            dict.ReadDictionary(StringIO("ATTRIBUTE Test-Type 1 dummy"))
+            self.dict.ReadDictionary(StringIO("ATTRIBUTE Test-Type 1 dummy"))
         except ParseError, e:
             self.assertEqual(e.linenumber, 1)
             self.assertEqual("dummy" in str(e), True)
 
 
     def testValueTooFewColumnsError(self):
-        dict=Dictionary()
-        self.assertRaises(ParseError, dict.ReadDictionary,
+        self.assertRaises(ParseError, self.dict.ReadDictionary,
                 StringIO("VALUE Oops-Too-Few-Columns"))
         try:
-            dict.ReadDictionary(StringIO("VALUE Oops-Too-Few-Columns"))
+            self.dict.ReadDictionary(StringIO("VALUE Oops-Too-Few-Columns"))
         except ParseError, e:
             self.assertEqual(e.linenumber, 1)
             self.assertEqual("value" in str(e), True)
 
 
     def testValueForUnknownAttributeError(self):
-        dict=Dictionary()
-        self.assertRaises(ParseError, dict.ReadDictionary,
+        self.assertRaises(ParseError, self.dict.ReadDictionary,
                 StringIO("VALUE Test-Attribute Test-Text 1"))
         try:
-            dict.ReadDictionary(StringIO("VALUE Test-Attribute Test-Text 1"))
+            self.dict.ReadDictionary(StringIO("VALUE Test-Attribute Test-Text 1"))
         except ParseError, e:
             self.assertEqual(e.linenumber, 1)
             self.assertEqual("unknown attribute" in str(e), True)
 
 
     def testIntegerValueParsing(self):
-        dict=Dictionary(os.path.join(self.path, "simple"))
-        self.assertEqual(len(dict["Test-Integer"].values), 0)
-        dict.ReadDictionary(StringIO("VALUE Test-Integer Value-Six 5"))
-        self.assertEqual(len(dict["Test-Integer"].values), 1)
+        self.assertEqual(len(self.dict["Test-Integer"].values), 0)
+        self.dict.ReadDictionary(StringIO("VALUE Test-Integer Value-Six 5"))
+        self.assertEqual(len(self.dict["Test-Integer"].values), 1)
         self.assertEqual(
-                DecodeAttr("integer", dict["Test-Integer"].values["Value-Six"]),
+                DecodeAttr("integer", self.dict["Test-Integer"].values["Value-Six"]),
                 5)
 
 
     def testStringValueParsing(self):
-        dict=Dictionary(os.path.join(self.path, "simple"))
-        self.assertEqual(len(dict["Test-String"].values), 0)
-        dict.ReadDictionary(StringIO("VALUE Test-String Value-Custard custardpie"))
-        self.assertEqual(len(dict["Test-String"].values), 1)
+        self.assertEqual(len(self.dict["Test-String"].values), 0)
+        self.dict.ReadDictionary(StringIO("VALUE Test-String Value-Custard custardpie"))
+        self.assertEqual(len(self.dict["Test-String"].values), 1)
         self.assertEqual(
-                DecodeAttr("string", dict["Test-String"].values["Value-Custard"]),
+                DecodeAttr("string", self.dict["Test-String"].values["Value-Custard"]),
                 "custardpie")
