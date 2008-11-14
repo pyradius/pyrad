db1a62212fedc84a946c35b7ceef17abb3eeb4db
Wichert Akkerman <wichert@wiggy.net>
2008-11-14 23:02:21 +0000

    Use the standard build directory

--- conf.py
+++ conf.py
@@ -57,7 +57,7 @@ today_fmt = '%B %d, %Y'
 
 # List of directories, relative to source directories, that shouldn't be searched
 # for source files.
-exclude_trees = ['.build']
+exclude_trees = ['build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
