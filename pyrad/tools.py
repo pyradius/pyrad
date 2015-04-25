6ac517009214bb425031096cb93fa18ec377d57c
Christian Giese <cgiese@juniper.net>
2015-04-25 13:37:51 +0200

    add support for ipv6addr

--- pyrad/tools.py
+++ pyrad/tools.py
@@ -34,6 +34,12 @@ def EncodeIPv6Prefix(addr):
     return struct.pack('2B', *[0, ip.prefixlen ]) + ip.ip.packed
 
 
+def EncodeIPv6Address(addr):
+    if not isinstance(addr, six.string_types):
+        raise TypeError('IPv6 Address has to be a string')
+    return IPAddress(addr).packed
+
+
 def EncodeInteger(num):
     if not isinstance(num, six.integer_types):
         raise TypeError('Can not encode non-integer as integer')
@@ -67,6 +73,12 @@ def DecodeIPv6Prefix(addr):
     return str(IPNetwork("%s/%s" % (prefix, int(length, 16))))
 
 
+def DecodeIPv6Address(addr):
+    addr = addr + '\x00' * (16-len(addr))
+    prefix = ':'.join(map('{:x}'.format, struct.unpack('!'+'H'*8, addr)))
+    return str(IPAddress(prefix)
+
+
 def DecodeInteger(num):
     return (struct.unpack('!I', num))[0]
 
@@ -80,12 +92,14 @@ def EncodeAttr(datatype, value):
         return EncodeString(value)
     elif datatype == 'octets':
         return EncodeOctets(value)
+    elif datatype == 'integer':
+        return EncodeInteger(value)
     elif datatype == 'ipaddr':
         return EncodeAddress(value)
     elif datatype == 'ipv6prefix':
         return EncodeIPv6Prefix(value)
-    elif datatype == 'integer':
-        return EncodeInteger(value)
+    elif datatype == 'ipv6addr':
+        return EncodeIPv6Address(value)
     elif datatype == 'date':
         return EncodeDate(value)
     else:
@@ -97,12 +111,14 @@ def DecodeAttr(datatype, value):
         return DecodeString(value)
     elif datatype == 'octets':
         return DecodeOctets(value)
+    elif datatype == 'integer':
+        return DecodeInteger(value)
     elif datatype == 'ipaddr':
         return DecodeAddress(value)
     elif datatype == 'ipv6prefix':
         return DecodeIPv6Prefix(value)
-    elif datatype == 'integer':
-        return DecodeInteger(value)
+    elif datatype == 'ipv6addr':
+        return DecodeIPv6Address(value)
     elif datatype == 'date':
         return DecodeDate(value)
     else:
