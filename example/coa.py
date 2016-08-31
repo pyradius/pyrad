aef95f92a388dd0f4127b196cf1d49a4211dea90
Christian Giese <cgiese@juniper.net>
2016-08-31 15:59:48 +0200

    add coa example

---
+++ example/coa.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+from __future__ import print_function
+from pyrad.client import Client
+from pyrad import dictionary
+
+ADDRESS = "127.0.0.1"
+SECRET = b"Kah3choteereethiejeimaeziecumi"
+ATTRIBUTES = {
+    "Acct-Session-Id": "1337"
+}
+# set code to 40 to send a disconnect request instead of coa (43)
+CODE = 43
+
+# create coa client
+client = Client(server=ADDRESS, secret=SECRET, authport=3799, acctport=3799, dict=dictionary.Dictionary("dictionary"))
+# set coa timeout
+client.timeout = 30
+
+# create coa request packet
+attributes = {k.replace("-", "_"): attributes[k] for k in attributes}
+request = client.CreateAcctPacket(code=CODE, **attributes)
+
+# send coa request
+result = client.SendPacket(request)
+print(result)
