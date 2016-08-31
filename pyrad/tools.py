b4d849b17b4e52fe129b9b7ab9b2dcb22681c18b
Christian Giese <cgiese@juniper.net>
2016-08-31 14:13:49 +0200

    fix CI error with python 3 + add #33 + change in ascend

--- pyrad/tools.py
+++ pyrad/tools.py
@@ -110,12 +110,11 @@ def EncodeAscendBinary(str):
             terms[key] = struct.pack('B', int(value))
 
     trailer = 8 * b'\x00'
-    result = b'%s%s%s\x00%s%s%s%s%s\x00%s%s%s%s\x00\x00%s' % (
+    return = b'%s%s%s\x00%s%s%s%s%s\x00%s%s%s%s\x00\x00%s' % (
         terms['family'], terms['action'], terms['direction'], terms['src'],
         terms['dst'], terms['srcl'], terms['dstl'], terms['proto'],
         terms['sport'], terms['dport'], terms['sportq'], terms['dportq'],
         trailer)
-    return result
 
 
 def EncodeInteger(num):
