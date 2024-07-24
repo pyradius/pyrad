9b7c823497df23051f7b833aa7b22a7607328b9e
Christian Giese <christian@rtbrick.com>
2024-07-24 20:53:20 +0000

    fix usage of socket.getaddrinfo

--- pyrad/client.py
+++ pyrad/client.py
@@ -73,7 +73,7 @@ class Client(host.Host):
 
     def _SocketOpen(self):
         try:
-            family = socket.getaddrinfo(self.server, 'www')[0][0]
+            family = socket.getaddrinfo(self.server, 80)[0][0]
         except:
             family = socket.AF_INET
         if not self._socket:
